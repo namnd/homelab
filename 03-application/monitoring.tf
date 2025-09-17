0019f0e7938c9ddcddab259116ff3264b7197f57
Nam Nguyen <me@namnd.com>
2025-09-17 22:10:38 +1000

    feat(03-app): add prometheus

--- 03-application/monitoring.tf
+++ 03-application/monitoring.tf
@@ -1,6 +1,10 @@
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
+
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+    }
   }
 }
 
@@ -9,7 +13,6 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   version    = "10.0.0"
-
 
   create_namespace = false
   namespace        = kubernetes_namespace.monitoring.id
@@ -20,5 +23,14 @@ resource "helm_release" "grafana" {
       value = true
     }
   ]
+}
 
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = "27.37.0"
+
+  create_namespace = false
+  namespace        = kubernetes_namespace.monitoring.id
 }
