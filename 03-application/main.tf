bdb25e84bff73f57e2e052a1fbdafeca845874cb
Nam Nguyen <me@namnd.com>
2025-12-11 12:08:38 +1000

    chore(03-app): pin k8s/helm provider

--- 03-application/main.tf
+++ 03-application/main.tf
@@ -30,6 +30,14 @@ terraform {
   }
 
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 5"
