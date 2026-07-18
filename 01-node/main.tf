fa2630656da889b72ca9256728d57196f657f9ba
Nam Nguyen <me@namnd.com>
2026-07-18 18:24:12 +1000

    feat(01-node): add new node with more disk space & remove iso for installed nodes

--- 01-node/main.tf
+++ 01-node/main.tf
@@ -8,18 +8,35 @@ locals {
       cpus      = 2
       memory    = 4096
       disk_size = 20480
+      state     = "installed"
     }
     "w1" = {
       role      = "worker"
       cpus      = 4
       memory    = 4096
-      disk_size = 204800
+      disk_size = 256000
+      state     = "installed"
     }
     "w2" = {
       role      = "worker"
       cpus      = 4
       memory    = 4096
-      disk_size = 204800
+      disk_size = 256000
+      state     = "installed"
+    }
+    "w3" = {
+      role      = "worker"
+      cpus      = 4
+      memory    = 4096
+      disk_size = 256000
+      state     = "installed"
+    }
+    "w4" = {
+      role      = "worker"
+      cpus      = 4
+      memory    = 4096
+      disk_size = 256000
+      state     = "installed"
     }
   }
 }
@@ -114,7 +131,7 @@ resource "virtualbox_vm" "this" {
 }
 
 resource "virtualbox_vm_storage_attachment" "iso_attachment" {
-  for_each = virtualbox_vm.this
+  for_each = { for k, v in virtualbox_vm.this : k => v if local.nodes[k].state != "installed" }
 
   vm_id           = each.value.id
   controller_name = "IDE Controller"
